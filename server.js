b2db6037398961d72a6fcc4fc248dd5036a67ade
Marco Nguyen <bao1230802@gmail.com>
2023-03-31 15:58:17 +0700

    testing modules in math

--- server.js
+++ server.js
@@ -11,15 +11,24 @@ console.log('Hello !!!'); // node server (write in terminal)
 const os = require('os');
 const path = require('path');
 
-console.log(os.type())
-console.log(os.version())
-console.log(os.homedir())
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
 
-console.log(__dirname)
-console.log(__filename)
+// console.log(__dirname)
+// console.log(__filename)
 
-console.log(path.dirname(__filename))
-console.log(path.basename(__filename))
-console.log(path.extname(__filename))
+// console.log(path.dirname(__filename))
+// console.log(path.basename(__filename))
+// console.log(path.extname(__filename))
 
-console.log(path.parse(__filename))
+// console.log(path.parse(__filename))
+
+// Tạo module ở file math.js
+// const math = require('./math');
+const {add, subtract, multiply, divide} = require('./math')
+
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(multiply(2, 3));
+console.log(divide(2, 3));
