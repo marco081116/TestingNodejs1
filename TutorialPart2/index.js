20dc6a1c241dda3dd0ac04a35126f4f48ffb30a6
Marco Nguyen <bao1230802@gmail.com>
2023-04-02 01:51:18 +0700

    read and write files

--- TutorialPart2/index.js
+++ TutorialPart2/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path')
 
 // Có 2 cách
  
@@ -7,7 +8,25 @@ const fs = require('fs');
 //     console.log(data.toString())
 // })
 
-fs.readFile('./files/starter.txt', 'utf-8', (err, data) => {
+// fs.readFile('./files/a.txt', 'utf-8', (err, data) => {
+//     if (err) throw err;
+//     console.log(data)
+// })
+
+
+fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8', (err, data) => {
     if (err) throw err;
     console.log(data)
+})
+
+// console.log('Hello) // Nó sẽ output trc rồi mới đọc file
+
+fs.writeFile(path.join(__dirname, 'files', 'end.txt'), 'Hello Marco !!!', (err) => {
+    if (err) throw err;
+    console.log('Write completed !!!')
+})
+
+process.on('uncaughtException', err =>{
+    console.error(`There was an uncaught error : ${err}`);
+    process.exit(1)
 })
