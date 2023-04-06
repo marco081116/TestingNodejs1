7477300f7b835d8adfb5a6443c2ff39313ee93f7
Marco Nguyen <bao1230802@gmail.com>
2023-04-06 21:02:41 +0700

    Done part 4

--- TutorialPart_3_4/index.js
+++ TutorialPart_3_4/index.js
@@ -5,3 +5,20 @@
 // // npm run dev
 // console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'))
 // console.log(uuid())
+
+// Part 4 - emit: phát ra
+const logEvents = require('./logEvents');
+const EventEmiiter = require('events');
+
+class MyEmitter extends EventEmiiter { };
+
+// initialize object
+const myEmitter = new MyEmitter();
+
+// add listener for the log event 
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(() => {
+    // Emit event
+    myEmitter.emit('log', 'Log event emitted !!!')
+}, 2000); // wait for 2 sec
--- TutorialPart_3_4/logEvents.js
+++ TutorialPart_3_4/logEvents.js
@@ -8,12 +8,15 @@ const path = require('path');
 
 const logEvents = async(message) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
-    const logItem = `${dateTime}\t${uuid()}\t${message}` // Để uuid vô để phân biệt các unique id cho từng các messages
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n` // Để uuid vô để phân biệt các unique id cho từng các messages
     console.log(logItem) 
     try {
+        if (!fs.existsSync(path.join(__dirname, 'logs'))) { // nếu chưa có thì tạo mới
+            await fsPromises.mkdir(path.join(__dirname, 'logs'))
+        }
         await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem);
     } catch (err) {
-        console.err(err);l
+        console.log(err);
     }
 }
 
