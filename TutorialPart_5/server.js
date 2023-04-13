dfb6eeab1d022884bb059be3a9b854d0fdb62ecf
Marco Nguyen <bao1230802@gmail.com>
2023-04-13 13:16:49 +0700

    let filePath ex

--- TutorialPart_5/server.js
+++ TutorialPart_5/server.js
@@ -18,15 +18,27 @@ const PORT = process.env.PORT || 3500
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
 
-    let filePath;
-    if (req.url === '/' || req.url === 'index.html') {
-        res.statusCode = 200 // means that the server has successfully processed the request and is returning the requested data as the response (chat GPT)
-        res.setHeader('Content-type', 'text/html');
-        filePath = path.join(__dirname, 'views', 'index.html');
-        fs.readFile(path, 'utf-8', (err, data => {
-            res.end(data) // The server then sends a plain text response of "Hello World" (chat GPT)
-        }))
-    }
+    // -- Dạng 1:
+    // let filePath;
+    // if (req.url === '/' || req.url === 'index.html') {
+    //     res.statusCode = 200 // means that the server has successfully processed the request and is returning the requested data as the response (chat GPT)
+    //     res.setHeader('Content-type', 'text/html');
+    //     filePath = path.join(__dirname, 'views', 'index.html');
+    //     fs.readFile(path, 'utf-8', (err, data => {
+    //         res.end(data) // The server then sends a plain text response of "Hello World" (chat GPT)
+    //     }))
+    // }
+
+    // -- Dạng 2:
+    // let filePath;
+    // switch(req.url) {
+    //     case('/'):
+    //         res.statusCode = 200;
+    //         filePath = path.join(__dirname, 'views', 'index.html');
+    //         fs.readFile(path, 'utf-8', (err, data => {
+    //         res.end(data) // The server then sends a plain text response of "Hello World" (chat GPT)
+    //         }))
+    // }
 })
 
 // -- chưa chạy server nên cần listen for request
