fe6393a500102c1dbb43adac23eace188a531b3a
Marco Nguyen <bao1230802@gmail.com>
2023-04-13 20:57:56 +0700

    Repair new file(s)

---
+++ TutorialPart_5_6/server_part5.js
@@ -0,0 +1,145 @@
+// -- include some common core modules
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+const fsPromises = require('fs').promises
+
+// -- part 3 4
+const logEvents = require('./logEvents');
+const EventEmiiter = require('events');
+class Emitter extends EventEmiiter { };
+// -- initialize object
+const myEmitter = new Emitter();
+
+// -- add listener for the log event 
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+
+// -- address of local host 
+const PORT = process.env.PORT || 3500
+
+// -- serveFile function cho `fileExists` 
+const serveFile = async (filePath, contentType, response) => {
+    try {
+        // const data = await fsPromises.readFile(filePath, 'utf-8');
+        const rawData = await fsPromises.readFile(
+            filePath, 
+            // 'utf-8'
+            !contentType.includes('image') ? 'utf-8' : '' // add hình ảnh
+        );
+        const data = contentType === 'application/json'
+            ? JSON.parse(rawData) : rawData;
+        
+        response.writeHead(
+            // 200 // nếu false thì trả về 404 chứ ko phải 200 
+            filePath.includes('404.html') ? 404 : 200, 
+            { 'Content-Type': contentType }
+        );
+        // response.end(data);
+        response.end(
+            contentType === 'application/json' 
+                ? JSON.stringify(data) : data
+        );
+    } catch(err){
+        console.log(err);
+        myEmitter.emit('log', `${err.name}\t${err.message}`, 'errLog.txt');
+        response.statusCode = 500;
+        response.end();
+    }
+}
+
+// -- define a port -> create a server
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method)
+    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+
+    // -- Dạng 1:
+    // let filePath;
+    // if (req.url === '/' || req.url === 'index.html') {
+    //     res.statusCode = 200 // means that the server has successfully processed the request and is returning the requested data as the response (chat GPT)
+    //     res.setHeader('Content-type', 'text/html');
+    //     filePath = path.join(__dirname, 'views', 'index.html');
+    //     fs.readFile(path, 'utf-8', (err, data => {
+    //         res.end(data) // The server then sends a plain text response of "Hello World" (chat GPT)
+    //     }))
+    // }
+
+    // -- Dạng 2:
+    // let filePath;
+    // switch(req.url) {
+    //     case('/'):
+    //         res.statusCode = 200;
+    //         filePath = path.join(__dirname, 'views', 'index.html');
+    //         fs.readFile(path, 'utf-8', (err, data => {
+    //         res.end(data) // The server then sends a plain text response of "Hello World" (chat GPT)
+    //         }))
+    // }
+
+    const extension = path.extname(req.url); // get the extension part in file (.txt, .css, .py)
+
+    let contentType; // define content type (.txt, .css, .py)
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    let filePath =
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+
+    // makes .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    const fileExists = fs.existsSync(filePath);
+    if (fileExists) {
+        // serve the file
+        serveFile(filePath, contentType, res);
+    } else {
+        // 404
+        // 301 redirect
+        switch (path.parse(filePath).base) { // .base return file name (/path/to/file.txt => file.txt)
+            case 'old-page.html':
+                res.writeHead(301, { 'Location': '/new-page.html' });
+                res.end();
+                break;
+            case 'www-page.html':
+                res.writeHead(301, { 'Location': '/' });
+                res.end();
+                break;
+            default:
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+        }
+    }
+})
+
+// -- chưa chạy server nên cần listen for request
+server.listen(PORT, () => console.log(
+    `Server runs on PORT ${PORT}`
+))
+
+
+
+    
