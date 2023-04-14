9f7dbf86208b7208fc620047e13dae242b38683c
Marco Nguyen <bao1230802@gmail.com>
2023-04-14 13:32:50 +0700

    Start part 7

---
+++ TutorialPart_5_6_7/server_part6.js
@@ -0,0 +1,73 @@
+// -- part 6
+const express = require('express'); // framework của node js
+const app = express();
+
+// -- include some common core modules
+const path = require('path')
+
+// -- address of local host 
+const PORT = process.env.PORT || 3500
+
+// -- 
+app.get('^/$|/index(.html)?', (req, res) => { 
+            // '^' => phải bắt đầu với /; '$' => phải kết thúc với /; '|' => toán tử OR 
+            // (.html)?  là optional
+    // res.send('Hello !!!');
+    // -- chuyền file vào
+    // -- cách  1:
+    // res.sendFile('./views/index.html', { root: __dirname })
+    // -- cách 2:
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+}) 
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html') // chuyển old thành new mãi mãi
+}) 
+
+// -- route handlers
+
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next()
+}, (req, res) => {
+    res.send('Hello world !!!')
+})
+
+//  -- chaining route handlers
+
+const one = (req, res, next) => {
+    console.log('one')
+    next()
+}
+
+const two = (req, res, next) => {
+    console.log('two')
+    next()
+}
+
+const three = (req, res) => {
+    console.log('three')
+    res.send('Finished !!!!')
+}
+
+app.get('/chain(.html)?', [one, two, three])
+
+// --
+
+app.get('/*', (req, res) => {
+    // res.sendFile(path.join(__dirname, 'views', '404.html')) // status vẫn 200, ko phải 404 lỗi thật nên cần phải custom lại
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+// -- chưa chạy server nên cần listen for request
+app.listen(PORT, () => console.log(
+    `Server runs on PORT ${PORT}`
+))
+
+
+
+    
