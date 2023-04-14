55782c559696a73e9ea797e1c8e1ef46e3fb6d85
Marco Nguyen <bao1230802@gmail.com>
2023-04-14 13:21:57 +0700

    route handlers

--- TutorialPart_5_6/server.js
+++ TutorialPart_5_6/server.js
@@ -9,7 +9,9 @@ const path = require('path')
 const PORT = process.env.PORT || 3500
 
 // -- 
-app.get('^/$|/index.html', (req, res) => { // '^' => phải bắt đầu với /; '$' => phải kết thúc với /; '|' => toán tử OR 
+app.get('^/$|/index(.html)?', (req, res) => { 
+            // '^' => phải bắt đầu với /; '$' => phải kết thúc với /; '|' => toán tử OR 
+            // (.html)?  là optional
     // res.send('Hello !!!');
     // -- chuyền file vào
     // -- cách  1:
@@ -18,11 +20,28 @@ app.get('^/$|/index.html', (req, res) => { // '^' => phải bắt đầu với /
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
-app.get('/new-page.html', (req, res) => {
+app.get('/new-page(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
 }) 
 
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html') // chuyển old thành new mãi mãi
+}) 
 
+// -- route handlers
+
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html');
+    next()
+}, (req, res) => {
+    res.send('Hello world !!!')
+    console.log('Test load data')
+})
+
+app.get('/*', (req, res) => {
+    // res.sendFile(path.join(__dirname, 'views', '404.html')) // status vẫn 200, ko phải 404 lỗi thật nên cần phải custom lại
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
 
 // -- chưa chạy server nên cần listen for request
 app.listen(PORT, () => console.log(
