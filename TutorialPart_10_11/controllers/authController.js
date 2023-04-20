707ce71731cb5b21b0beca699f7c39e7b96ab8e5
Marco Nguyen <bao1230802@gmail.com>
2023-04-20 16:08:48 +0700

    authController.js done

--- TutorialPart_10_11/controllers/authController.js
+++ TutorialPart_10_11/controllers/authController.js
@@ -8,6 +8,15 @@ const usersDB = {
 
 const bcrypt = require('bcrypt')
 
+// -- part 11
+
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+const fsPromises = require('fs').promises
+const path = require('path')
+
+// -- part 10
+
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body
     if (!user || !pwd) {
@@ -22,8 +31,35 @@ const handleLogin = async (req, res) => {
     const match = await bcrypt.compare(pwd, foundUser.password)
 
     if (match) {
+        // -- part 11
+        const accessToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s' } // Thường là 10mins 15mins, trong này chỉ làm 30s để thấy được
+        )
+        const refreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' } // Thường là 10mins 15mins, trong này chỉ làm 30s để thấy được
+        )
+        
+        // -- saving refresh token with current user
+        const otherUsers = usersDB.users.filter(
+            person => person.username !== foundUser.username
+        )
+        const currentUser = { ...foundUser,refreshToken}
+        usersDB.setUsers([...otherUsers, currentUser])
+        await fsPromises.writeFile(
+            path.join(__dirname, '..', 'model', 'user.json'),
+            JSON.stringify(usersDB.users)
+        )
+        //  cookie có thể bị gây hại trong JS, nhưng với dạng http thì khác
+        res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 }) 
+            // lưu theo millisecond
+        res.json({ accessToken })
+        // -- part 10 
         // create JWTs (next part)
-        res.json({'success': `User ${user} is logged in !!!`})
+        // res.json({'success': `User ${user} is logged in !!!`})
     }
     else {
         return res.sendStatus(401) // 401 => unauthorized
