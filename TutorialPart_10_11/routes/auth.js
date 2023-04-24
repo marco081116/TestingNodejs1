a7811f9aefbf1d2654256ee480e5a3b91cf2d863
Marco Nguyen <bao1230802@gmail.com>
2023-04-24 22:29:10 +0700

    add more

--- TutorialPart_10_11/routes/auth.js
+++
@@ -1,8 +0,0 @@
-// -- part 10
-const express = require('express')
-const router = express.Router()
-const authController = require('../controllers/authController')
-
-router.post('/', authController.handleLogin)
-
-module.exports = router
--- TutorialPart_10_11/routes/refresh.js
+++
@@ -1,8 +0,0 @@
-// -- part 11
-const express = require('express')
-const router = express.Router()
-const refreshTokenController = require('../controllers/refreshTokenController')
-
-router.get('/', refreshTokenController.handleRefreshToken)
-
-module.exports = router
