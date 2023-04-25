e8834d83c70d647a6d7954ed35d4eb5071e70238
Marco Nguyen <bao1230802@gmail.com>
2023-04-25 14:41:41 +0700

    working on employees in api

--- TutorialPart_10_11_12/routes/api/employees.js
+++ TutorialPart_10_11_12/routes/api/employees.js
@@ -6,15 +6,20 @@ const employeeController = require('../../controllers/employeesController')
 // -- part 11
 // const verifyJWT = require('../../middleware/verifyJWT')
 
+// -- part 12
+const ROLES_LIST = require('../../config/roles_list')
+const verifyRoles = require('../../middleware/verifyRoles')
+
 router.route('/')
     // --  Đã cắt code ra ném qua bên employeeController.js cho dễ nhìn
     // .get(verifyJWT, employeeController.getAllEmployees) 
         // chỗ này nghĩa là sau khi verify mới get employees
         // cái này dừng dể chọn route cụ thể để bảo vệ => đóng lại, có cách nhanh hơn
     .get(employeeController.getAllEmployees) 
-    .post(employeeController.createNewEmployee)
-    .put(employeeController.updateEmployee)
-    .delete(employeeController.deleteEmployee)
+    // -- part 12 => thêm chức năng chỉ admin hay editor được làm điều đó
+    .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeeController.createNewEmployee)
+    .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), employeeController.updateEmployee)
+    .delete(verifyRoles(ROLES_LIST.Admin), employeeController.deleteEmployee)
 
 router.route('/:id')
     .get(employeeController.getEmployee)
--- TutorialPart_10_11_12/routes/auth.js
+++ TutorialPart_10_11_12/routes/auth.js
@@ -3,6 +3,7 @@ const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController')
 
+
 router.post('/', authController.handleLogin)
 
 module.exports = router
