89dfe15efeea5778530cba82343ba7fe624eb74c
Marco Nguyen <bao1230802@gmail.com>
2023-04-25 14:36:36 +0700

    working on verify JWT and Roles middleware

--- TutorialPart_10_11_12/middleware/verifyJWT.js
+++ TutorialPart_10_11_12/middleware/verifyJWT.js
@@ -1,12 +1,15 @@
+// -- part 11
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    if(!authHeader) {
+    // const authHeader = req.headers['authorization'];
+    // -- part 12
+    const authHeader = req.headers.authorization || req.headers.Authorization; // cho trường hợp trả về A in
+    if(!authHeader?.startsWith('Bearer ')) { // nếu có authHeader rồi mà nó vẫn ko startsWith 'Bearer ' thì lỗi form vẫn là lỗi
         return res.sendStatus(401)
     }
-    console.log(authHeader) // chỗ này sẽ output 'bearer token'
+    // console.log(authHeader) // chỗ này sẽ output 'bearer token'
     const token = authHeader.split(' ')[1] // nếu đúng sẽ ra bearer token và lấy phần tử thứ 1(token)
     jwt.verify(
         token,
@@ -15,7 +18,10 @@ const verifyJWT = (req, res, next) => {
             if (err) {
                 return res.sendStatus(403) // => invalid token
             }
-            req.user = decoded.username
+            // req.user = decoded.username
+            // -- part 12
+            req.user = decoded.UserInfo.username // Do đã sửa lại thành UserInfo 
+            req.roles = decoded.UserInfo.roles 
             next()
         }
     )
---
+++ TutorialPart_10_11_12/middleware/verifyRoles.js
@@ -0,0 +1,21 @@
+const verifyRoles = (...allowedRoles) => { // Chỗ này ta cho truyền vào nhiều tham số
+    return (req, res, next) => {
+        if (!req?.roles) {
+            return res.sendStatus(401)
+        } // nếu ko có request và nếu có request mà ko có roles
+        const rolesArray = [...allowedRoles]
+        console.log(rolesArray)
+        console.log(req.roles)
+        const res = req.roles.map(
+            role => rolesArray.includes(role)
+        ).find(
+            val => val === true
+        )
+        if (!res) {
+            return res.sendStatus(401)
+        }
+        next()
+    }
+}
+
+module.exports = verifyRoles 
